976e06fbe1e43038fc23d3e63a9506d2de239f9b
Gerrit-Jan Kaal <gjkaal@outlook.com>
2024-08-06 12:06:27 +0200

    Modify default table name

--- src/N2.LoggingService/AzureLogServiceSettings.cs
+++ src/N2.LoggingService/AzureLogServiceSettings.cs
@@ -5,5 +5,5 @@ public class AzureLogServiceSettings
     public string AppName { get; set; } = "AzureLogService";
     public string ConnectionString { get; set; } = string.Empty;
     public string LogCategory { get; set; } = string.Empty;
-    public string TableName { get; set; } = "n2logging";
+    public string TableName { get; set; } = "N2logging";
 }
